d8c8c03623b4e31bba0d20d75b94bd31b3df9232
Gerson Rocha <gersonrocha9@gmail.com>
2025-10-04 17:16:30 -0300

    chore(types): adicionar EditProperty aos tipos de navegação

---
+++ src/types/navigation.ts
@@ -0,0 +1,42 @@
+import { NavigatorScreenParams } from '@react-navigation/native'
+
+// Auth Stack
+export type AuthStackParamList = {
+  Login: undefined
+  SignUp: undefined
+}
+
+// Property Stack (after login)
+export type PropertyStackParamList = {
+  SelectProperty: undefined
+  AddProperty: undefined
+  EditProperty: { propertyId: string }
+  PropertyTabs: NavigatorScreenParams<PropertyTabsParamList>
+  CoffeeDetail: { id: string }
+  PlotDetail: { id: string }
+  AddAccount: undefined
+  AddCoffee: undefined
+  AddPlot: { coffeeId: string }
+  AddActivity: { plotId: string }
+}
+
+// Property Tabs
+export type PropertyTabsParamList = {
+  Home: undefined
+  Financial: undefined
+  Harvest: undefined
+  Production: undefined
+  Profile: undefined
+}
+
+// Root Stack
+export type RootStackParamList = {
+  Auth: NavigatorScreenParams<AuthStackParamList>
+  Property: NavigatorScreenParams<PropertyStackParamList>
+}
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
